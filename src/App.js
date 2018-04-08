7b8dbab2647e2280a6ace1cfca3c1da66639f4f7
Klara <fleischmann.kl@gmail.com>
2018-04-08 22:11:26 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src="/imgs/chuck_norris_dancin___by_jesgrad07.gif"/>
+        <h1>Whoops, there's nothing here yet.</h1>
+        <p>By the way: Chuck Norris doesn't need try-catch, exceptions are too afraid to raise.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// By the way: Chuck Norris doesn't need try-catch, exceptions are too afraid to raise.
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+
+import './App.css';
+
+render(<App />, document.querySelector('#root'));
