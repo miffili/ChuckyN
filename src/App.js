1701172b5b6474b17d022f21cf3e0ee2fba28eb1
Klara <fleischmann.kl@gmail.com>
2018-04-08 22:29:10 +0200

    fix dancing Chuck

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import conterfey from './chuck_norris_dancin___by_jesgrad07.gif';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src="/imgs/chuck_norris_dancin___by_jesgrad07.gif"/>
+        <img src={conterfey}/>
         <h1>Whoops, there's nothing here yet.</h1>
         <p>By the way: Chuck Norris doesn't need try-catch, exceptions are too afraid to raise.</p>
       </div>
