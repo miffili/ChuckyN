d2c78464fb6d07734e61390e734a4c162c18af3e
Klara <fleischmann.kl@gmail.com>
2018-04-12 20:38:27 +0200

    Add basic Chuck Norris joke fetching function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: [],
+      count: 3
+    }
+    this.onButtonRandom = this.onButtonRandom.bind(this);
+  }
+
+  onButtonRandom() {
+    const endpoint = `http://api.icndb.com/jokes/random/${this.state.count}`;
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(data => this.setState({result: data.value}));
+  }
+
   render() {
+    // console.log(this.state);
     return (
       <div className="App">
-        <img src="imgs/chuck_norris_dancin___by_jesgrad07.gif"/>
-        <h1>Whoops, there's nothing here yet.</h1>
-        <p>By the way: Chuck Norris doesn't need try-catch, exceptions are too afraid to raise.</p>
+        <img src="imgs/conterfey.png"/>
+        <JokeButton random={this.onButtonRandom}/>
+        {this.state.result !== [] ?
+          <FactDisplay result={this.state.result}/>
+        : ''}
       </div>
     );
   }
+}
+
+const FactDisplay = (props) => {
+  console.log(props.result)
+  return (
+    props.result.map((el, i) =>
+      <div className="fact" key={`${el.id}_${i}`}>
+        <p>{el.joke}</p>
+        <a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(el.joke)}`}><i className="fab fa-twitter"></i> Tweet</a>
+      </div>
+  ));
+}
+
+const JokeButton = (props) => {
+  return (
+    <button onClick={props.random}>Hit me!</button>
+  );
 }
 
 export default App;
