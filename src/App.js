27cf5f01f15695ea04a8bca83f18bf83e46a38c3
Klara <fleischmann.kl@gmail.com>
2018-05-23 12:00:58 +0200

    Fix fetch error

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
       lastname: "",
       onlyDev: false,
       visible: false
-    }
+    };
     this.onJokeButton = this.onJokeButton.bind(this);
     this.onClearButton = this.onClearButton.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -26,25 +26,29 @@ class App extends React.Component {
     const value = !this.state.visible;
     this.setState({
       visible: value
-    })
+    });
   }
 
   onJokeButton(event) {
-    const addFirstname = this.state.firstname !== "" ? `&firstName=${this.state.firstname}` : "";
-    const addLastname = this.state.lastname !== "" ? `&lastName=${this.state.lastname}` : "";
+    const addFirstname =
+      this.state.firstname !== "" ? `&firstName=${this.state.firstname}` : "";
+    const addLastname =
+      this.state.lastname !== "" ? `&lastName=${this.state.lastname}` : "";
     const limitToDev = this.state.onlyDev === true ? `&limitTo=[nerdy]` : "";
-    const endpoint = `http://api.icndb.com/jokes/random/${this.state.count}?exclude=[explicit]&escape=javascript${addFirstname}${addLastname}${limitToDev}`;
+    const endpoint = `https://api.icndb.com/jokes/random/${
+      this.state.count
+    }?exclude=[explicit]&escape=javascript${addFirstname}${addLastname}${limitToDev}`;
 
     const result = this.state.result;
 
-    if(event.target.name === "more") {
+    if (event.target.name === "more") {
       fetch(endpoint)
-      .then(response => response.json())
-      .then(data => this.setState({result: [...result, ...data.value]}))
+        .then(response => response.json())
+        .then(data => this.setState({ result: [...result, ...data.value] }));
     } else {
       fetch(endpoint)
-      .then(response => response.json())
-      .then(data => this.setState({result: data.value}))
+        .then(response => response.json())
+        .then(data => this.setState({ result: data.value }));
     }
   }
 
@@ -56,7 +60,7 @@ class App extends React.Component {
       lastname: "",
       onlyDev: false,
       settingVisible: false
-    })
+    });
   }
 
   handleSubmit(event) {
@@ -72,64 +76,54 @@ class App extends React.Component {
 
   handleCheckboxChange() {
     const value = !this.state.onlyDev;
-    this.setState({onlyDev: value});
+    this.setState({ onlyDev: value });
   }
 
   render() {
-    const hitMe = `Hit me${this.state.result !== "" ? " again" : ""}!`
+    const hitMe = `Hit me${this.state.result !== "" ? " again" : ""}!`;
     return (
       <div className="App">
-        <img src="imgs/conterfey.png" alt="monochromatic head of chuck norris"/>
+        <img
+          src="imgs/conterfey.png"
+          alt="monochromatic head of chuck norris"
+        />
 
         <div className="menu">
-          <button
-            onClick={ this.onJokeButton }
-            tabIndex="1"
-          >
-            { hitMe }
+          <button onClick={this.onJokeButton} tabIndex="1">
+            {hitMe}
           </button>
-          <button
-            onClick={this.onShowSettings}
-            tabIndex="1"
-          >
-            Settings
-            {' '}
-            {this.state.visible ?
-              <i className="fas fa-caret-up"></i> :
-              <i className="fas fa-caret-down"></i>}
+          <button onClick={this.onShowSettings} tabIndex="1">
+            Settings{" "}
+            {this.state.visible ? (
+              <i className="fas fa-caret-up" />
+            ) : (
+              <i className="fas fa-caret-down" />
+            )}
           </button>
         </div>
-        {this.state.visible &&
+        {this.state.visible && (
           <Settings
-            count={ this.state.count }
-            firstname={ this.state.firstname }
-            lastname={ this.state.lastname }
-            onlyDev={ this.state.onlyDev }
-            handleSubmit={ this.handleSettingsChange }
-            handleSettingsChange={ this.handleSettingsChange }
-            handleCheckboxChange={ this.handleCheckboxChange}
-          />}
+            count={this.state.count}
+            firstname={this.state.firstname}
+            lastname={this.state.lastname}
+            onlyDev={this.state.onlyDev}
+            handleSubmit={this.handleSettingsChange}
+            handleSettingsChange={this.handleSettingsChange}
+            handleCheckboxChange={this.handleCheckboxChange}
+          />
+        )}
 
-        {this.state.result !== "" &&
-          <FactDisplay
-            result={ this.state.result }
-          />}
-        {this.state.result !== "" &&
+        {this.state.result !== "" && <FactDisplay result={this.state.result} />}
+        {this.state.result !== "" && (
           <div>
-            <button
-              onClick={ this.onJokeButton }
-              tabIndex="3"
-              name="more"
-            >
-              Give me { this.state.count } more!
+            <button onClick={this.onJokeButton} tabIndex="3" name="more">
+              Give me {this.state.count} more!
             </button>
-            <button
-              onClick={ this.onClearButton }
-              tabIndex="3"
-            >
+            <button onClick={this.onClearButton} tabIndex="3">
               Clear all!
             </button>
-          </div>}
+          </div>
+        )}
       </div>
     );
   }
